069177343a1dbf687b8ed5ba18d9abea3f38819a
luis-david <luissmith@hotmail.fr>
2024-07-18 15:43:37 +0200

    beug

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const final = [];
+    for (const value of array) {
+      final.push(appendString + value);
+    }
+  
+    return final;
+  }
+    
