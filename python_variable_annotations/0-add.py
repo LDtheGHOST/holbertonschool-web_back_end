64566961f8387d376aa224c6b4d5b2fa53c46fd3
LDtheGHOST <luissmith@hotmail.fr>
2024-12-06 20:48:45 +0100

    scadere et you

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-"""
-    0-add.py
-"""
+'''Module for add two floats'''
 
 
 def add(a: float, b: float) -> float:
-    """
-        Add two floats and return a float.
-    """
+    '''Add two floats and return the result'''
     return a + b
