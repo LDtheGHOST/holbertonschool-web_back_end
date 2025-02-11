579615b5075b8948c7b0c62a47506ff4a207c943
LDtheGHOST <luissmith@hotmail.fr>
2025-02-11 17:14:06 +0100

    correction

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,16 @@
+// 4-http.js
 const http = require('http');
 
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
 });
 
 module.exports = app;
